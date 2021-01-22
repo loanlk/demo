5f2bc754eb78951d7148503ac37222b1700c36c4
midori <lchauloan@gmail.com>
2021-01-22 20:50:30 +0700

    update test data

--- src/test/java/tests/AddUserTests.java
+++ src/test/java/tests/AddUserTests.java
@@ -14,12 +14,12 @@ public class AddUserTests extends BaseTest {
         authPage.goToAuthPage()
                 .login("lemidori16@gmail.com", "D0r@3m0n")
                 .goToEngagePage().goToUserPage().goToAddUser()
-                .sendTextToInputControl("firstName",1,"Victoria3")
-                .sendTextToInputControl("lastName",1,"Beckham3")
-                .sendTextToInputControl("email",1,"vic.beck3@gmail.com")
-                .sendTextToInputControl("firstName",2,"Parris3")
-                .sendTextToInputControl("lastName",2,"Hilton3")
-                .sendTextToInputControl("email",2,"parris.hilton3@gmail.com")
+                .sendTextToInputControl("firstName",1,"Victoria4")
+                .sendTextToInputControl("lastName",1,"Beckham4")
+                .sendTextToInputControl("email",1,"vic.beck4@gmail.com")
+                .sendTextToInputControl("firstName",2,"Parris4")
+                .sendTextToInputControl("lastName",2,"Hilton4")
+                .sendTextToInputControl("email",2,"parris.hilton4@gmail.com")
                 .clickAddPeople().verifyAddPeopleSuccessfully("check_circle\n" +
                 "Congratulations");
     }
